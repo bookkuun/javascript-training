52470bb2ba5c5622222c3d57a48094f58a2eff49
bookkuun <kuboyuya@outlook.jp>
2021-10-05 10:39:39 +0900

    add: todoリスト

---
+++ todolist/index.js
@@ -0,0 +1,54 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => {
+    add(todo);
+  });
+}
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(input.value);
+  add();
+});
+
+function add(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo;
+  }
+
+  if (todoText.length > 0) {
+    const li = document.createElement("li");
+    li.innerText = todoText;
+    li.classList.add("list-group-item");
+
+    li.addEventListener("contextmenu", function (event) {
+      event.preventDefault();
+      li.remove();
+      saveData();
+    });
+
+    li.addEventListener("click", function () {
+      li.classList.toggle("text-decoration-line-through");
+    });
+
+    ul.appendChild(li);
+    saveData();
+  }
+}
+
+function saveData() {
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+  lists.forEach((list) => {
+    todos.push(list.innerText);
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
