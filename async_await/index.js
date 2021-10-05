a5c1a49f6d62b2e908ebb30dc67f1ab2a0995b19
bookkuun <kuboyuya@outlook.jp>
2021-10-05 12:24:01 +0900

    add: XMLHttpRequestの利用

--- async_await/index.js
+++ async_await/index.js
@@ -18,3 +18,16 @@ function callApi() {
 }
 
 callApi();
+
+function callApiXML() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+  xhr.responseType = "json";
+  xhr.send();
+
+  xhr.onload = function () {
+    console.log(xhr.response);
+  };
+}
+
+callApiXML();
