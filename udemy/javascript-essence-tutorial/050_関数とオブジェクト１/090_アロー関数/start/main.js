3a451a1550c2a916641ad1ccd45f06204830ffc6
bookkuun <kuboyuya@outlook.jp>
2021-11-23 14:58:33 +0900

    add: アロー関数

---
+++ udemy/javascript-essence-tutorial/050_関数とオブジェクト１/090_アロー関数/start/main.js
@@ -0,0 +1,16 @@
+function a(name) {
+  return "hello " + name;
+}
+
+const b = function (name) {
+  return "hello " + name;
+};
+
+// アロー関数
+
+const c = (name) => {
+  return "hello " + name;
+};
+
+console.log(b("Tom"));
+console.log(c("Jhon"));
