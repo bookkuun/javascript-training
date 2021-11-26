2861ebc9436d81b7a066e6f002c86d77d5b3fae8
bookkuun <kuboyuya@outlook.jp>
2021-11-26 18:47:18 +0900

    add: 練習問題4.2

--- dokushu/chapter4/lesson4.1/index.js
+++ dokushu/chapter4/lesson4.1/index.js
@@ -1,6 +1,1 @@
-let a = 0;
-let b = 0;
-a = 1 + (b = 10);
-
-console.log(a);
-console.log(b);
+console.log(2 * 3 ** 2);
---
+++ dokushu/chapter4/lesson4.2/index.js
@@ -0,0 +1,10 @@
+console.log(2 * 3 ** 2);
+console.log(10 / 2 + (3 - 2));
+console.log(10 / (2 + 3) - 2);
+
+let a = 1;
+console.log(a++);
+
+let b = 10;
+let c = 1;
+console.log(--b * ++c);
