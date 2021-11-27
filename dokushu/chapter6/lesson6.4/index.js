8c7ef75f4d3358595caec23dd42d530827a56fb2
bookkuun <kuboyuya@outlook.jp>
2021-11-27 15:04:30 +0900

    add: 練習問題6.4

---
+++ dokushu/chapter6/lesson6.4/index.js
@@ -0,0 +1,16 @@
+// 関数宣言
+console.log(`3 + 5 = ${sum(3, 5)}`);
+
+function sum(val1, val2) {
+  return val1 + val2;
+}
+console.log(`10 + 7 = ${sum(10, 7)}`);
+
+// 関数式
+// console.log(`3 + 5 = ${sum(3, 5)}`); // エラーが発生！
+
+// // エラーが発生したため、これ以降は実行されない
+// const sum = function (val1, val2) {
+//   return val1 + val2;
+// };
+// console.log(`10 + 7 = ${sum(10, 7)}`);
