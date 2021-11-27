a3370af3e714cf7300ba95a19060d131d46ccdd3
bookkuun <kuboyuya@outlook.jp>
2021-11-27 10:45:32 +0900

    add: 練習問題5.3

---
+++ dokushu/chapter5/lesson5.3/index.js
@@ -0,0 +1,13 @@
+let animal = "ゾウ"; // 変数の初期値はなんでもよい
+
+switch (animal) {
+  case "ゾウ":
+    console.log("ゾウ");
+  case "ウマ":
+    console.log("ウマ");
+  case "ウサギ":
+    console.log("ウサギ");
+    break;
+  default:
+    console.log("何かわかりません。");
+}
