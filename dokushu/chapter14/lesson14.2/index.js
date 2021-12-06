a4a7cba8580f9941ed37dc344b5fa10ca1cee556
bookkuun <kuboyuya@outlook.jp>
2021-12-06 20:38:06 +0900

    add: 練習問題14.2

---
+++ dokushu/chapter14/lesson14.2/index.js
@@ -0,0 +1,5 @@
+const a = document.querySelector("#textContent");
+const b = document.querySelector("#innerHTML");
+
+a.textContent = "<strong>textContentとinnerHTML</strong>";
+b.innerHTML = "<strong>textContentとinnerHTML</strong>";
