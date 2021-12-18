91537cb61e1ca4fc3a397290622f0a2beb720027
bookkuun <kuboyuya@outlook.jp>
2021-12-18 11:05:13 +0900

    add: getterとsetter

--- vue/udemy/practice/index.js
+++ vue/udemy/practice/index.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
   data: () => ({
-    message: "Hello Vue.js",
+    basePrice: 100,
   }),
   computed: {
-    reverseMessage: function () {
-      return this.message.split("").reverse().join("");
-    },
-  },
-  methods: {
-    reverseMessageMethod: function () {
-      return this.message.split("").reverse().join("");
+    taxIncludedPrice: {
+      get: function () {
+        return this.basePrice * 1.1;
+      },
+      // set: function (value) {
+      //   this.basePrice = value / 1.1;
+      // },
     },
   },
 });
