9b00b4268681176d89b395c1545afb32e2d1d654
bookkuun <kuboyuya@outlook.jp>
2021-12-18 16:33:46 +0900

    add: add: p44

--- vue/vue_sample/PART1/Todolist/app.js
+++ vue/vue_sample/PART1/Todolist/app.js
@@ -3,38 +3,7 @@ Vue.createApp({
     return {
       todoTitle: "",
       todoDescription: "",
-      todoCategories: [],
-      selectedCategory: "",
-      hideDoneTodo: false,
       searchWord: "",
-      order: "desc",
-      categoryName: "",
     };
-  },
-  computed: {
-    canCreateTodo: function () {
-      return this.todoTitle !== "";
-    },
-    canCreateCategory: function () {
-      return this.categoryName !== "";
-    },
-  },
-  methods: {
-    createTodo: function () {
-      if (!this.canCreateTodo) {
-        return;
-      }
-
-      this.todoTitle = "";
-      this.todoDescription = "";
-      this.todoCategories = [];
-    },
-    canCreateCategory: function () {
-      if (!this.canCreateCategory) {
-        return;
-      }
-
-      this.categoryName = "";
-    },
   },
 }).mount("#app");
