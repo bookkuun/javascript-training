5d0c95b56b663cf34ba318e9c500b9a7b63be81e
bookkuun <kuboyuya@outlook.jp>
2021-12-19 10:27:58 +0900

    add: p96

--- vue/vue_sample/PART1/Todolist/app.js
+++ vue/vue_sample/PART1/Todolist/app.js
@@ -38,6 +38,14 @@ Vue.createApp({
       return this.categories.indexOf(categoryName) !== -1;
     },
   },
+  watch: {
+    todos: {
+      handler: function (next) {
+        window.localStorage.setItem("todos", JSON.stringify(next));
+      },
+      deep: true,
+    },
+  },
   methods: {
     //Todoの作成
     createTodo: function () {
@@ -71,5 +79,17 @@ Vue.createApp({
 
       this.categoryName = "";
     },
+  },
+  created: function () {
+    const todos = window.localStorage.getItem("todos");
+    const categories = window.localStorage.getItem("categories");
+
+    if (todos) {
+      this.todos = JSON.parse(todos);
+    }
+
+    if (categories) {
+      this.categories = JSON.parse(categories);
+    }
   },
 }).mount("#app");
