fd4a2c090bf901ae62a6597209d96ef6fd146839
bookkuun <kuboyuya@outlook.jp>
2021-12-20 22:22:04 +0900

    update: 練習問題3.4

--- javascript/dokushu/chapter3/lesson3.4/index.js
+++ javascript/dokushu/chapter3/lesson3.4/index.js
@@ -1,4 +1,12 @@
-console.log(5 + 6 - 1);
-console.log(5 + 6 - 1);
-console.log(((5 + 6 - 1) * 2) % 3);
-console.log((((5 + 6 - 1) * 2) % 3) ** 3);
+let sum = 5 + 6 - 1;
+console.log(sum);
+// 10
+sum = sum * 2; // 10 * 2
+console.log(sum);
+// 20
+sum = sum % 3; // 20 / 3の余り2
+console.log(sum);
+// 2
+sum = sum ** 3; // 2 * 2 * 2 = 8
+console.log(sum);
+// 8
