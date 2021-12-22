ab97335cdfb940e33f3eb91b76aab6e447d5f8dd
bookkuun <kuboyuya@outlook.jp>
2021-12-22 09:56:00 +0900

    add: Todoの追加機能

--- javascript/todolist/index.js
+++ javascript/todolist/index.js
@@ -1,1 +1,21 @@
-console.log("a");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+form.addEventListener("submit", function (event) {
+  // formを実行するとリロードされてしまうから
+  event.preventDefault();
+  console.log(input.value);
+  add();
+});
+
+function add() {
+  let todoText = input.value;
+  if (todoText.length > 0) {
+    const li = document.createElement("li");
+    li.textContent = todoText;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    input.value = "";
+  }
+}
