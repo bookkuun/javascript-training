cd6540c978656dfc024a453cec9893d6466cf23a
bookkuun <kuboyuya@outlook.jp>
2022-01-09 11:29:58 +0900

    add: トランプゲームのデザイン(13)

--- javascript/recursion/medium/リスト/トランプカード/index.js
+++ javascript/recursion/medium/リスト/トランプカード/index.js
@@ -12,18 +12,22 @@ class Card {
 }
 
 class Deck {
-  constructor() {
-    this.deck = Deck.generateDeck();
+  constructor(gameMode = null) {
+    this.deck = Deck.generateDeck(gameMode);
   }
 
-  static generateDeck() {
+  static generateDeck(gameMode = null) {
     let newDeck = [];
     const suits = ["♣", "♦", "♥", "♠"];
     const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+    const blackJack = { A: 1, J: 10, Q: 10, K: 10 };
+
     for (let i = 0; i < suits.length; i++) {
       for (let j = 0; j < values.length; j++) {
-        newDeck.push(new Card(suits[i], values[j], j + 1));
+        let currentValue = values[j];
+        let intValue = gameMode === "21" ? (currentValue in blackJack ? blackJack[currentValue] : parseInt(currentValue)) : j + 1;
+        newDeck.push(new Card(suits[i], values[j], intValue));
       }
     }
 
@@ -139,6 +143,7 @@ class HelperFunctions {
     return maxIndex;
   }
 }
+
 let table = Dealer.startGame(4, "21");
 
 Dealer.printTableInformation(table);
