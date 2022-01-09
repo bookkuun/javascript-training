52ae1d56526d27a5eb9ef7d27bc457ed70b4a8f2
bookkuun <kuboyuya@outlook.jp>
2022-01-09 10:33:37 +0900

    add: トランプゲームのデザイン(4)

--- javascript/recursion/medium/リスト/トランプカード/index.js
+++ javascript/recursion/medium/リスト/トランプカード/index.js
@@ -40,3 +40,23 @@ class Deck {
 
 let deck1 = new Deck();
 deck1.printDeck();
+
+//カードのシャッフル
+//Math.random --- 0以上1未満のランダムな数
+console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.floor(Math.random() * 10));
+
+let intArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = intArr.length - 1; i >= 0; i--) {
+  let j = Math.floor(Math.random() * (i + 1));
+  // jというランダムな数字とiの位置にある数字を交換を繰り返している
+  // temporary(仮)の意味
+  // i番目をtempに保存し、i番目をj番目で更新し、j番目をtempで更新します。
+  let temp = intArr[i];
+  intArr[i] = intArr[j];
+  intArr[j] = temp;
+}
+
+console.log(intArr);
