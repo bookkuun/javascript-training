37f4b39a5fc8cdd54c654fea3bf2ed79a0645ddd
bookkuun <kuboyuya@outlook.jp>
2022-01-09 09:55:07 +0900

    add: トランプゲームのデザイン(1)

---
+++ javascript/recursion/medium/リスト/トランプカード/index.js
@@ -0,0 +1,16 @@
+class Card {
+  constructor(suit, value, intValue) {
+    this.suit = suit;
+    this.value = value;
+    this.intValue = intValue;
+  }
+
+  //インスタンス化されたカード情報を返す関数
+  getCardString() {
+    return this.suit + this.value + "(" + this.intValue + ")";
+  }
+}
+
+let card1 = new Card("♦︎", "A", 1);
+console.log(card1);
+console.log(card1.getCardString());
