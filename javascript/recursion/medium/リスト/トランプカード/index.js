7aa1423ab04ef18cfbbfae317f5b40d5036e534a
bookkuun <kuboyuya@outlook.jp>
2022-01-09 10:04:05 +0900

    add: トランプゲームのデザイン(2)

--- javascript/recursion/medium/リスト/トランプカード/index.js
+++ javascript/recursion/medium/リスト/トランプカード/index.js
@@ -11,6 +11,34 @@ class Card {
   }
 }
 
+class Deck {
+  constructor() {
+    this.deck = Deck.generateDeck();
+  }
+
+  static generateDeck() {
+    let newDeck = [];
+    const suits = ["♣", "♦", "♥", "♠"];
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < values.length; j++) {
+        newDeck.push(new Card(suits[i], values[j], j + 1));
+      }
+    }
+
+    return newDeck;
+  }
+}
+
 let card1 = new Card("♦︎", "A", 1);
 console.log(card1);
 console.log(card1.getCardString());
+
+let deck1 = new Deck();
+console.log(deck1);
+
+// コンソールで確認
+console.log(deck1.deck);
+console.log(deck1.deck[3]);
+console.log(deck1.deck[3].getCardString());
