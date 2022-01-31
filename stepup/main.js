e6fc2e30f889d70969f716db78739b03cbdde4b1
bookkuun <kuboyuya@outlook.jp>
2022-01-31 15:54:11 +0900

    add: カウントアップ

--- stepup/main.js
+++ stepup/main.js
@@ -1,1 +1,11 @@
-alert("test");
+var displayElm = document.getElementsByClassName("display")[0];
+var startButton = document.getElementsByClassName("startButton")[0];
+
+startButton.addEventListener("click", function () {
+  console.log("start");
+  var seconds = 0;
+  setInterval(function () {
+    seconds++;
+    displayElm.innerHTML = seconds;
+  }, 1000);
+});
