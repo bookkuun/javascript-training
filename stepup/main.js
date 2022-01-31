474d0e5bd7ca7bd2df0407bdd3f2dbec548ef6e0
bookkuun <kuboyuya@outlook.jp>
2022-01-31 16:23:55 +0900

    add: if分の追加

--- stepup/main.js
+++ stepup/main.js
@@ -1,11 +1,24 @@
 var displayElm = document.getElementsByClassName("display")[0];
+var timer = null;
+
 var startButton = document.getElementsByClassName("startButton")[0];
 
 startButton.addEventListener("click", function () {
-  console.log("start");
-  var seconds = 0;
-  setInterval(function () {
-    seconds++;
-    displayElm.innerHTML = seconds;
-  }, 1000);
+  if (timer === null) {
+    var seconds = 0;
+    timer = setInterval(function () {
+      seconds++;
+      displayElm.innerHTML = seconds;
+    }, 1000);
+    console.log("start:" + timer);
+  }
+});
+
+var stopButton = document.getElementsByClassName("stopButton")[0];
+stopButton.addEventListener("click", function () {
+  if (timer !== null) {
+    console.log("stop:" + timer);
+    clearInterval(timer);
+    timer = null;
+  }
 });
