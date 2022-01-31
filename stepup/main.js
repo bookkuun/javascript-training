53f852716494a3d94c816c80ec4b37a2fe1bbfd7
bookkuun <kuboyuya@outlook.jp>
2022-01-31 16:51:26 +0900

    add: ストップウォッチ

--- stepup/main.js
+++ stepup/main.js
@@ -1,24 +1,49 @@
-var displayElm = document.getElementsByClassName("display")[0];
-var timer = null;
-
-var startButton = document.getElementsByClassName("startButton")[0];
-
-startButton.addEventListener("click", function () {
-  if (timer === null) {
-    var seconds = 0;
-    timer = setInterval(function () {
-      seconds++;
-      displayElm.innerHTML = seconds;
-    }, 1000);
-    console.log("start:" + timer);
+function stopWatch(options) {
+  function addMessage(message) {
+    var messageElm = document.createElement("div");
+    var now = new Date();
+    messageElm.innerText = now.getHours() + "時" + now.getMinutes() + "分" + now.getSeconds() + "秒 " + message;
+    messageElm.classList = "message";
+    logElm.appendChild(messageElm);
   }
-});
 
-var stopButton = document.getElementsByClassName("stopButton")[0];
-stopButton.addEventListener("click", function () {
-  if (timer !== null) {
-    console.log("stop:" + timer);
-    clearInterval(timer);
-    timer = null;
-  }
-});
+  options = options || {};
+  var color = options.color || "lightblue";
+  var backgroundColor = options.backgroundColor || "black";
+  var displayElm = document.getElementsByClassName("display")[0];
+  displayElm.style.color = color;
+  displayElm.style.backgroundColor = backgroundColor;
+
+  var logElm = document.querySelector(".log");
+  var timer = null;
+
+  var startButton = document.getElementsByClassName("startButton")[0];
+
+  startButton.addEventListener("click", function () {
+    if (timer === null) {
+      var seconds = 0;
+      timer = setInterval(function () {
+        seconds++;
+        displayElm.innerHTML = seconds;
+      }, 1000);
+      addMessage("開始");
+      console.log("start:" + timer);
+    }
+  });
+
+  var stopButton = document.getElementsByClassName("stopButton")[0];
+  stopButton.addEventListener("click", function () {
+    if (timer !== null) {
+      console.log("stop:" + timer);
+      clearInterval(timer);
+      timer = null;
+      addMessage("終了");
+    }
+  });
+}
+
+var options = {
+  color: "limegreen",
+  backgroundColor: "#333",
+};
+stopWatch(options);
