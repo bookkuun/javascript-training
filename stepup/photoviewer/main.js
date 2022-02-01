1f2ad48260783cc96c11ce49094c7a391eed4fbd
bookkuun <kuboyuya@outlook.jp>
2022-02-01 10:16:45 +0900

    add: photoPrever完成

--- stepup/photoviewer/main.js
+++ stepup/photoviewer/main.js
@@ -6,14 +6,60 @@ class PhotoViewer {
   }
 
   init() {
-    const frameElm = this.rootElm.querySelector(".frame");
-    const image = this.images[this.currentIndex];
+    const nextButtonElm = this.rootElm.querySelector(".nextButton");
+    nextButtonElm.addEventListener("click", () => {
+      this.next();
+    });
 
+    const prevButtonElm = this.rootElm.querySelector(".prevButton");
+    prevButtonElm.addEventListener("click", () => {
+      this.prev();
+    });
+
+    this.updatePhoto();
+  }
+
+  updatePhoto() {
+    const frameElm = this.rootElm.querySelector(".frame");
+    const imageIndex = this.currentIndex + 1;
     frameElm.innerHTML = `
-      <div class="currentImage">
-        <img src= "${image}" />
+      <div>
+        <p>${imageIndex}枚目</p>
+        <img src= "${this.images[this.currentIndex]}" />
       </div>
     `;
+
+    this.startTimer();
+  }
+
+  startTimer() {
+    if (this.timerKey) {
+      clearTimeout(this.timerKey);
+    }
+
+    this.timerKey = setTimeout(() => {
+      this.next();
+    }, 3000);
+  }
+
+  next() {
+    const lastIndex = this.images.length - 1;
+    if (lastIndex === this.currentIndex) {
+      this.currentIndex = 0;
+    } else {
+      this.currentIndex++;
+    }
+    this.updatePhoto();
+  }
+
+  prev() {
+    const lastIndex = this.images.length - 1;
+    if (this.currentIndex === 0) {
+      this.currentIndex = lastIndex;
+    } else {
+      this.currentIndex--;
+    }
+    this.updatePhoto();
   }
 }
 
