a40c6d91044128dc87aea3b7931ddd2dd818eccb
bookkuun <kuboyuya@outlook.jp>
2022-02-04 10:46:19 +0900

    add: tapple

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,7 +1,13 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "yota",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
 let favoriteActivities: string[];
