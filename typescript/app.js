4804f457d3c6b5b800ac10dabad504525fac2e6f
bookkuun <kuboyuya@outlook.jp>
2022-02-04 10:40:48 +0900

    add: Array

--- typescript/app.js
+++ typescript/app.js
@@ -1,5 +1,11 @@
 var person = {
     name: "yota",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
 };
-console.log(person);
+var favoriteActivities;
+favoriteActivities = ["Sports"];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,6 +1,12 @@
 const person = {
   name: "yota",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
-console.log(person.name);
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
