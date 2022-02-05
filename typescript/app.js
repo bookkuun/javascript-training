31e07d55acca4412f4fa18c5cad2fdcf9ba501eb
bookkuun <kuboyuya@outlook.jp>
2022-02-05 16:45:15 +0900

    add: calback

--- typescript/app.js
+++ typescript/app.js
@@ -4,4 +4,11 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result" + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
 printResult(add(5, 12));
