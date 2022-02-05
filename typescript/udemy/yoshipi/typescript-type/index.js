95575f87189bc08bbeaebd660bb7b1ad3ef4c9d6
bookkuun <kuboyuya@outlook.jp>
2022-02-05 20:05:31 +0900

    add: object

---
+++ typescript/udemy/yoshipi/typescript-type/index.js
@@ -0,0 +1,10 @@
+// 基本的に型推論使用する
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -12;
+var string = "hello";
+var person = {
+    name: "Jack",
+    age: 21
+};
---
+++ typescript/udemy/yoshipi/typescript-type/index.ts
@@ -0,0 +1,14 @@
+// 基本的に型推論使用する
+let hasValue = true;
+let count = 10;
+let float = 3.14;
+let negative = -12;
+let string = "hello";
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Jack",
+  age: 21,
+};
