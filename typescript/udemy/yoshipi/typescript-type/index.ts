f6a6633df2dffa09b1e5f41b7391f394a16ada09
bookkuun <kuboyuya@outlook.jp>
2022-02-05 20:19:30 +0900

    add: タプル型

--- typescript/udemy/yoshipi/typescript-type/index.ts
+++ typescript/udemy/yoshipi/typescript-type/index.ts
@@ -1,14 +1,23 @@
 // 基本的に型推論使用する
+
+// 型
 let hasValue = true;
 let count = 10;
 let float = 3.14;
 let negative = -12;
-let string = "hello";
+let string = 'hello';
 
+//オブジェクト
 const person: {
   name: string;
   age: number;
 } = {
-  name: "Jack",
+  name: 'Jack',
   age: 21,
 };
+
+//配列
+const fruits: string[] = ['Apple', 'Banana', 'Grape'];
+
+//タプル型(pushでは入れることができる)
+const book: [string, number, boolean] = ['business', 1500, false];
