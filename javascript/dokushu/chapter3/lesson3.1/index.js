8517ed14cff363934a60577a99cdb4bc1fbf8798
bookkuun <kuboyuya@outlook.jp>
2022-03-21 19:34:17 +0900

    fix: コードの場所を変更

--- javascript/dokushu/chapter3/lesson3.1/index.js
+++ javascript/dokushu/chapter3/lesson3.1/index.js
@@ -1,8 +1,8 @@
 // ［1］ letは値の再代入が可能ですが、constは値の再代入を行うことはできません。
 
-const fruit = "apple";
-fruit = "banana";
-console.log(fruit);
-
 // ［2］
 // エラーは2 行目（fruit = "banana";）で発生します。constを使っているので、値の再代入ができないため、エラーが発生します。
+
+const fruit = 'apple';
+fruit = 'banana';
+console.log(fruit);
