0b3906b330e1ad0e7364cd2b3674dc7e5c9d4f5a
bookkuun <bookkuun@gmail.com>
2022-04-26 14:42:46 +0900

    再レンダリングの仕組み

---
+++ react/my-app/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useCallback } from 'react';
+import { ChildArea } from './ChildArea';
+import './styles.css';
+
+export const App = () => {
+  console.log('App');
+
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (event) => {
+    setText(event.target.value);
+  };
+
+  const onClickOpen = () => {
+    setOpen(!open);
+  };
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  return (
+    <div className='App'>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+};
