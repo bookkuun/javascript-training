bb29eba64d0701956392810a5ebb21a3979c6d43
bookkuun <bookkuun@gmail.com>
2022-05-02 18:27:00 +0900

    typescript x react

--- react/my-app/src/App.tsx
+++ react/my-app/src/App.tsx
@@ -1,5 +1,26 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './styles.css';
+import { Todo } from './Todo';
+import { TodoType } from './types/todo';
+import { Text } from './Text';
 
 export const App = () => {
-  return <div className='App'></div>;
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios.get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos').then((res) => {
+      setTodos(res.data);
+    });
+  };
+
+  return (
+    <div className='App'>
+      <Text color='red' fontSize='18px' />
+      <button onClick={onClickFetchData}>データの取得</button>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed} />
+      ))}
+    </div>
+  );
 };
