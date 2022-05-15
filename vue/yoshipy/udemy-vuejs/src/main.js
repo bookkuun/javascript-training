9265dd77a4a6d4491145d283f7db6d4259fba575
bookkuun <bookkuun@gmail.com>
2022-05-15 14:13:27 +0900

    vuexの基本

--- vue/yoshipy/udemy-vuejs/src/main.js
+++ vue/yoshipy/udemy-vuejs/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 // import LikeNumber from './components/LikeNumber.vue';
 
@@ -10,6 +11,7 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
   // 上記と同じ意味
   // render: function (h) {
--- vue/yoshipy/udemy-vuejs/src/views/MyHome.vue
+++ vue/yoshipy/udemy-vuejs/src/views/MyHome.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
     <h3>Home</h3>
+    <p>{{ count }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
+};
+</script>
