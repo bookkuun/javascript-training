db0d2e07d716531aa96433af65884c4ba886c3ad
bookkuun <bookkuun@gmail.com>
2022-05-31 10:14:37 +0900

    vuexの基本

--- vue/yoshipy/udemy-vuejs/src/views/HeaderHome.vue
+++ vue/yoshipy/udemy-vuejs/src/views/HeaderHome.vue
@@ -3,8 +3,20 @@
     <h2>Home</h2>
     <router-link to="/" active-class="link--active" exact class="link">Home</router-link>
     <router-link to="/users" active-class="link--active" exact class="link">Users</router-link>
+    <button @click="increment(2)">+1</button>
+    <button @click="decrement(2)">-1</button>
   </nav>
 </template>
+
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+  methods: {
+    ...mapMutations(['increment', 'decrement']),
+  },
+};
+</script>
 
 <style scoped>
 .link {
