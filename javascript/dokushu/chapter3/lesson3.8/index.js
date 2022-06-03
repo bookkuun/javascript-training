8fd4c5fff0545d896cc2241220565dda19220af8
bookkuun <bookkuun@gmail.com>
2022-06-03 10:46:10 +0900

    空白

--- javascript/dokushu/chapter3/lesson3.8/index.js
+++ javascript/dokushu/chapter3/lesson3.8/index.js
@@ -1,10 +1,10 @@
 console.log(1 + true);
-//2
+// 2
 console.log(false + true);
-//1
+// 1
 console.log(Boolean(0));
-//false
+// false
 console.log(Boolean(1));
-//true
+// true
 console.log(Boolean(-1));
-//true
+// true
